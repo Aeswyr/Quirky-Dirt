900f7c8e0e7663331e230b56ec2effb9aa661ecb
pascal <pmkgbwis@gmail.com>
2023-10-08 16:48:24 -0700

    rolling?

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,11 +8,25 @@ public class PlayerController : NetworkBehaviour
 
     [SerializeField] private Rigidbody2D rbody;
     [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Animator animator;
+    
+
+
+    [Header("Stats")]
     [SerializeField] private float baseSpeed;
+    [SerializeField] private AnimationCurve rollCurve;
+    [SerializeField] private AnimationCurve controlOverrideWeight;
+    [SerializeField] private float redirectGraceWindow;
 
 
+    [SyncVar(hook = nameof(SyncFacing))] bool flipX;
 
-    [SyncVar(hook = nameof(UpdateFacing))] bool flipX;
+
+    private bool acting = false;
+    private Vector2 lastDir;
+    private float curveStartTime;
+    private AnimationCurve activeCurve = null;
+    private float redirectTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,28 +37,66 @@ public class PlayerController : NetworkBehaviour
     void FixedUpdate() {
         if (!isLocalPlayer)
             return;
-            
-        rbody.velocity = baseSpeed * InputHandler.Instance.dir;
 
-        bool facing = InputHandler.Instance.dir.x < 0;
-        if (InputHandler.Instance.dir.x != 0 && flipX != facing) {
-            sprite.flipX = facing;
-            SyncFacing(facing);
+        if (acting && Time.time < redirectTimer) {
+            lastDir = InputHandler.Instance.dir;
+            UpdateFacing();
         }
+        
+        if (!acting && InputHandler.Instance.move.down) {
+            lastDir = InputHandler.Instance.dir;
+            rbody.velocity = baseSpeed * lastDir;
+            
+            UpdateFacing();
+        } else if (activeCurve != null) {
+            float controlOverride = controlOverrideWeight.Evaluate(Time.time - curveStartTime);
+            Vector2 avgDir = (1 - controlOverride) * lastDir + controlOverride * InputHandler.Instance.dir;
+            rbody.velocity =  activeCurve.Evaluate(Time.time - curveStartTime) * baseSpeed * avgDir;
+        } else
+            rbody.velocity = Vector2.zero;
+        
+        if (!acting && InputHandler.Instance.dodge.pressed) {
+            animator.SetTrigger("dodge");
+
+            ToAction();
+            activeCurve = rollCurve;
+            curveStartTime = Time.time;
+        }
+        
 
         
 
 
 
-        if (InputHandler.Instance.pause.down)
+        if (InputHandler.Instance.pause.pressed)
             Application.Quit();
     }
 
-    private void UpdateFacing(bool oldFacing, bool newFacing) {
+    public void ToAction() {
+        acting = true;
+        redirectTimer = Time.time + redirectGraceWindow;
+    }
+
+    public void FromAction() {
+        if (!isLocalPlayer)
+            return;
+        activeCurve = null;
+        acting = false;
+    }
+
+    public void UpdateFacing() {
+        bool facing = InputHandler.Instance.dir.x < 0;
+        if (InputHandler.Instance.dir.x != 0 && flipX != facing) {
+            sprite.flipX = facing;
+            SendFacing(facing);
+        }
+    }
+
+    private void SyncFacing(bool oldFacing, bool newFacing) {
         sprite.flipX = newFacing;
     }
 
-    [Command] public void SyncFacing(bool facing) {
+    [Command] public void SendFacing(bool facing) {
         flipX = facing;
     }
 }
